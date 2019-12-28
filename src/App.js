346bb3fc4a1e73e93522dd220332b3de482ae7d1
slawekradzyminski <slawomir.radzyminski@gmail.com>
2019-12-28 23:43:06 +0100

    Improved UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import AppRouter from "./component/RouterComponent";
+import NavBar from "./component/Navbar";
+import Container from '@material-ui/core/Container';
 
 function App() {
-  return (
-      <div className="container">
-        <AppRouter/>
-      </div>
-  );
+    return (
+        <div>
+            <NavBar/>
+            <Container>
+                <AppRouter/>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
